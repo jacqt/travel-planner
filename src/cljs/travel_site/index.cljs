8ccab30273cd1ca9a4d651929d58bd6a88a299e7
Anthony Guo <anthony.guo@some.ox.ac.uk>
2015-11-26 09:16:46 +0000

    Combine landing page with landing dashboard to make logging in unrequired

--- src/cljs/travel_site/index.cljs
+++ src/cljs/travel_site/index.cljs
@@ -6,11 +6,10 @@
             [cljs.core.async :refer [put! chan <!]]
             [travel-site.components.dashboard :as dashboard]
             [travel-site.components.city :as city]
-            [travel-site.components.landing-page :as landing-page]
             [travel-site.utils.http :as http]
             [travel-site.utils.auth :as auth]))
 
-(defn index-logged-in-view [state owner]
+(defn index-view [state owner]
   (reify
     om/IRenderState
     (render-state [this _]
@@ -18,11 +17,3 @@
         "home" (om/build dashboard/dashboard-view state)
         "city" (om/build city/city-view state)
         (comment default) (om/build dashboard/dashboard-view state)))))
-
-(defn index-view [state owner]
-  (reify
-    om/IRenderState
-    (render-state [this _]
-      (if (empty? (@state :credentials))
-        (om/build landing-page/landing-page-view state)
-        (om/build index-logged-in-view state)))))
